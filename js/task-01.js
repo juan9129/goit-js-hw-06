4c0b9e9e3ba735ca382b03dc426928eadadb625b
Juan D Adames <juandadames@gmail.com>
2023-04-28 11:20:17 -0500

    task-1

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+const categoriesList = document.querySelector('#categories');
+const categoriesItems = categoriesList.querySelectorAll('.item');
+
+console.log(`Number of categories: ${categoriesItems.length}\n`);
+
+categoriesItems.forEach(item => {
+  const title = item.querySelector('h2').textContent;
+  const elementsCount = item.querySelectorAll('li').length;
+
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${elementsCount}\n`);
+});
