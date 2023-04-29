85157a132ab5917d00a690dfafb0f90ebe5714f9
Juan D Adames <juandadames@gmail.com>
2023-04-28 21:46:05 -0500

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  
+  if (!email || !password) { 
+    alert('Todos los espacios deben ser rellenados.');
+    return;
+  }
+  
+  const data = { 
+    email,
+    password,
+  };
+  
+  console.log(data); 
+  form.reset(); 
+});
